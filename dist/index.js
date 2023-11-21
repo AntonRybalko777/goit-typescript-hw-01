10a3ec9f11aae9762be120bebdb510e036c40095
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-11-21 15:14:04 +0200

    2

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 if (button && input) {
   button.addEventListener("click", () => {
-    concatenation(input.value, "hello!");
+    concatenation(input.value, "Hello!");
   });
 }
 //# sourceMappingURL=index.js.map
